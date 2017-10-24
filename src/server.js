d991c2c7ae91eb145cc280ca03fd2333622ab913
Maciej Samoraj <maciej.samoraj@gmail.com>
2017-10-24 20:39:31 +0200

    app url changed to oauth client redirect url

--- src/server.js
+++ src/server.js
@@ -23,7 +23,7 @@ const PORT = process.env.PORT || 4000
 const COOKIE_PASSWORD = process.env.COOKIE_PASSWORD || 'it-should-have-min-32-characters'
 const NODE_ENV = process.env.NODE_ENV || 'development'
 const MONGO_URL = process.env.MONGO_URL || 'mongodb://localhost:27017/vientos-idp'
-const VIENTOS_PWA_URL = process.env.VIENTOS_PWA_URL || 'http://localhost:8080'
+const OAUTH_CLIENT_REDIRECT_URI = process.env.OAUTH_CLIENT_REDIRECT_URI || 'http://localhost:3000/auth/vientos'
 
 mongoose.Promise = global.Promise
 mongoose.connect(MONGO_URL, { promiseLibrary: global.Promise })
@@ -31,8 +31,8 @@ mongoose.connect(MONGO_URL, { promiseLibrary: global.Promise })
 const server = new Hapi.Server()
 
 Handlebars.registerHelper('i18n', str => (i18n !== undefined ? i18n.__(str) : str))
-Handlebars.registerHelper('vientosPwaUrl', () => {
-  return VIENTOS_PWA_URL
+Handlebars.registerHelper('oauthClientRedirectUri', (str) => {
+  return OAUTH_CLIENT_REDIRECT_URI
 })
 
 i18n.configure({
